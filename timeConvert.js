aa7c9e0f838d29c7ea001a758e8ab3d94fcf5f82
Ahnming Loke <ahntagonist@gmail.com>
2014-07-07 18:28:13 -0700

    Create timeConvert.js

---
+++ timeConvert.js
@@ -0,0 +1,15 @@
+/*
+
+Using the JavaScript language, have the function TimeConvert(num) take the num
+parameter being passed and return the number of hours and minutes the parameter
+converts to (ie. if num = 63 then the output should be 1:3). Separate the number
+of hours and minuteswith a colon. 
+
+*/
+
+var timeConvert = function(num){
+  var hr = Math.floor(num / 60);
+  var min = num % 60;
+  total = hr + ":" + min;
+  return total;
+};
