24a40a93963dd588f99108db3ff18db2a2355029
Ahnming Loke <ahntagonist@gmail.com>
2014-07-07 18:37:44 -0700

    Create alphabetSoup.js

---
+++ alphabetSoup.js
@@ -0,0 +1,13 @@
+/*
+
+Using the JavaScript language, have the function AlphabetSoup(str) take the
+str string parameter being passed and return the string with the letters in
+alphabetical order (ie. hello becomes ehllo). Assume numbers and punctuation
+symbols will not be included in the string. 
+
+*/
+
+var aSoup = function(str){
+  var charArray = str.split('').sort().join('');
+  return charArray;
+};
