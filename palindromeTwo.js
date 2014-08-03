6d3a1f8c43ff0eeba0b58520102d0afe0f19efe2
Ahnming Loke <ahntagonist@gmail.com>
2014-08-03 11:02:50 -0700

    Update and rename palindrome.js to palindromeTwo.js

--- palindromeTwo.js
+++ palindromeTwo.js
@@ -1,4 +1,4 @@
-var palindrome = function(str) {
+var palindromeTwo = function(str) {
   for (var i = 0; i < str.length / 2; i++) {
     if (str[i] != str[str.length - 1 - i]) {
       return false;
