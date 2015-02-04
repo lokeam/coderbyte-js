11cce692e4da06765f221124994b701dcb2962eb
Ahnming Loke <lokeam@users.noreply.github.com>
2015-02-04 17:39:50 -0500

    reoptimized string scramble

--- stringScramble.js
+++ stringScramble.js
@@ -1,17 +1,12 @@
-var stringScramble = function(str1,str2){
-  var current='',
-      contained ='',
-      i,
-      j;
-  for (i = 0; i < str1.length; i++) {
-    current = str1[i];
-    for (j = 0; j < str2.length; j++) {
-      if (current.indexOf(str2) != 0) {
-        contained += str2[j];
-      }
-      if (contained.length == str2.length ) {
-        return true;
-      }
-    }
+function StringScramble(str1,str2) {
+  var char_index = 0;
+  for (var i = 0; i < str2.length; i++) {
+    char_index = str1.indexOf(str2.charAt(i));
+  if ( char_index === -1) {
+    return false;
+  } else {
+  str1 = str1.substring(0, char_index) + str1.substring(char_index + 1);
   }
-};
+}
+return true;
+}
